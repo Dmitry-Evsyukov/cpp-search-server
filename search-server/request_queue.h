dd759c302d5fa3d9b0673b7ff97d8aba5efbc865
My Name <edp032017@yandex.ru>
2022-07-07 13:18:51 +0300

    4 spint, project in different files sec

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -4,7 +4,6 @@
 class RequestQueue {
 public:
     explicit RequestQueue(const SearchServer& search_server);
-
     template <typename DocumentPredicate>
     std::vector<Document> AddFindRequest(const std::string &raw_query, DocumentPredicate document_predicate);
 
