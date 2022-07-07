610d74919d0032bc819360b562fa35d415971a8d
My Name <edp032017@yandex.ru>
2022-07-07 13:17:22 +0300

    4 spint, project in different files second try

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -4,41 +4,9 @@
 class RequestQueue {
 public:
     explicit RequestQueue(const SearchServer& search_server);
-    
-    template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        std::vector<Document> documents = search_server_.FindTopDocuments(raw_query, document_predicate);
-        if (requests_.size() < min_in_day_) {
-            if (documents.empty()) {
-                ++empty_results_;
-                requests_.push_back(QueryResult(requests_.size() + 1, false));
-            } else {
-                requests_.push_back(QueryResult(requests_.size() + 1, true));
-            }
-        } else {
-            if (documents.empty()) {
-                if (requests_.front().result == true) {
-                    requests_.pop_front();
-                    ++empty_results_;
-                    requests_.push_back(QueryResult(requests_.size() + 1, false));
-                } else {
-                    requests_.pop_front();
-                    requests_.push_back(QueryResult(requests_.size() + 1, false));
-                }
-            } else {
-                if (requests_.front().result == true) {
-                    requests_.pop_front();
-                    requests_.push_back(QueryResult(requests_.size() + 1, true));
-                } else {
-                    requests_.pop_front();
-                    --empty_results_;
-                    requests_.push_back(QueryResult(requests_.size() + 1, true));
-                }
-            }
-        }
 
-        return documents;
-    }
+    template <typename DocumentPredicate>
+    std::vector<Document> AddFindRequest(const std::string &raw_query, DocumentPredicate document_predicate);
 
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
 
@@ -56,5 +24,40 @@ private:
     const SearchServer& search_server_;
     int empty_results_;
 };
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    std::vector<Document> documents = search_server_.FindTopDocuments(raw_query, document_predicate);
+    if (requests_.size() < min_in_day_) {
+        if (documents.empty()) {
+            ++empty_results_;
+            requests_.push_back(QueryResult(requests_.size() + 1, false));
+        } else {
+            requests_.push_back(QueryResult(requests_.size() + 1, true));
+        }
+    } else {
+        if (documents.empty()) {
+            if (requests_.front().result == true) {
+                requests_.pop_front();
+                ++empty_results_;
+                requests_.push_back(QueryResult(requests_.size() + 1, false));
+            } else {
+                requests_.pop_front();
+                requests_.push_back(QueryResult(requests_.size() + 1, false));
+            }
+        } else {
+            if (requests_.front().result == true) {
+                requests_.pop_front();
+                requests_.push_back(QueryResult(requests_.size() + 1, true));
+            } else {
+                requests_.pop_front();
+                --empty_results_;
+                requests_.push_back(QueryResult(requests_.size() + 1, true));
+            }
+        }
+    }
+
+    return documents;
+}
 
 
